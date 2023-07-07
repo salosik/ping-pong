cc5a9fe46bdddf4a8a8ffbb8c4dd01d4114cbaa2
salosik <alphingold8@gmail.com>
2023-07-07 23:38:59 +0300

    cosmetics

---
+++ ping_pong.py
@@ -0,0 +1,23 @@
+from pygame import *    
+from random import randint
+
+WIDTH = 600
+HEIGHT = 500
+FPS = 60
+
+
+
+bg = ((0,175,70))
+window = display.set_mode((WIDTH,HEIGHT))
+display.set_caption('ping-pong')
+clock = time.Clock()
+
+run = True 
+while run :
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+
+    window.fill(bg)
+    display.update()
+    clock.tick(FPS)
